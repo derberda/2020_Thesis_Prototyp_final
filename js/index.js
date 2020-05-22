c9ac5a99db72c7c36a1e81d5dc189a3ced6b0c44
Berdan Der <derberdan@gmail.com>
2020-05-22 15:10:56 +0200

    Implement animations and visibility of hotspots
    Mittels TweenMax wird das 3D-Objekt je nach ausgewählter Information (Karusell) animiert, um die entsprechende Detail-Ansicht zu erhalten. Die dazu gehörenden Hotspots werden dementsprechend eingeblendet oder verschwinden.

--- js/index.js
+++ js/index.js
@@ -41,6 +41,11 @@ init = () => {
         shoe = gltf.scene.children[0];
         scene.position.set(0, -.05, 0);
 
+        //Hotspot am Anfang verstecken
+        for (let i = 5; i <= 10; i++) {
+            shoe.parent.children[i].material.visible = false;
+        }
+
     })
 
     container.appendChild(renderer.domElement);
@@ -55,6 +60,80 @@ animate = () => {
 
 init();
 
+//3D-Modell laden und Hotspots verstecken
+getActiveStatus = () => {
+    $('#carouselExampleIndicators').on('slide.bs.carousel', function onSlide(ev) {
+        let id = ev.relatedTarget.id;
+
+        switch (id) {
+            //normale Ansicht vorne
+            case "info_1":
+                console.log("test: 1")
+                TweenMax.to(shoe.rotation, 1, { x: -1.570796461153735, y: 0, z: 0 });
+                TweenMax.to(camera.position, 1, { x: -1, y: 0, z: 0 });
+
+                showHotspot();
+                break;
+            //Ansicht der Schuhzunge
+            case "info_2":
+                console.log("test: 2")
+                TweenMax.to(shoe.rotation, 1, { x: -1.570796461153735, y: 0, z: 0 });
+                TweenMax.to(shoe.parent.children[5].rotation, 1, { x: -1.570796461153735, y: 0, z: 0 });
+                TweenMax.to(camera.position, 1, { x: -.4, y: .05, z: 0 });
+
+                showHotspot(5);
+                break;
+            //Anischt Öse
+            case "info_3":
+                console.log("test: 3")
+                TweenMax.to(shoe.rotation, 1, { z: 1 });
+                TweenMax.to(shoe.parent.children[6].rotation, 1, { y: .09 });
+                TweenMax.to(camera.position, 1, { x: -.8, y: .05, z: 0 });
+
+                showHotspot(6)
+                break;
+            //Ansicht der seitlichen Löcher
+            case "info_4":
+                console.log("test: 4")
+                TweenMax.to(shoe.rotation, 1, { z: 1.3 });
+                // TweenMax.to(shoe.parent.children[7].rotation, 1, { z: 1.3 });
+                TweenMax.to(shoe.parent.children[7].rotation, 1, { y: .09 });
+                TweenMax.to(camera.position, 1, { x: -.8, y: .02, z: 0 });
+
+                showHotspot(7);
+                break;
+            //Ansicht der Nähte
+            case "info_5":
+                console.log("test: 5")
+                TweenMax.to(shoe.rotation, 1, { x: -1.570796461153735, y: 0, z: 0 });
+                TweenMax.to(shoe.parent.children[8].rotation, 1, { x: -1.570796461153735, y: 0, z: 0 });
+                TweenMax.to(camera.position, 1, { x: -.7, y: .01, z: 0 });
+
+                showHotspot(8);
+                break;
+            //Ansicht der Schuhsohle
+            case "info_6":
+                console.log("test: 6")
+                TweenMax.to(shoe.rotation, 1, { x: -1.570796461153735, y: 0.5, z: 0 });
+                TweenMax.to(shoe.parent.children[10].rotation, 1, { x: -1.570796461153735, y: 0.5, z: 0 });
+                TweenMax.to(camera.position, 1, { x: -.45, y: 0, z: 0 });
+
+                showHotspot(10)
+                break;
+            //Ansicht von Hinten
+            case "info_7":
+                console.log("test: 7")
+                TweenMax.to(shoe.rotation, 1, { x: -1.570796461153735, y: 0, z: 3.2 });
+                TweenMax.to(shoe.parent.children[9].rotation, 1, { x: -1.570796461153735, y: 0, z: 3.2 });
+                TweenMax.to(camera.position, 1, { x: -1, y: 0, z: 0 });
+
+                showHotspot(9);
+                break;
+            default:
+                break;
+        }
+    })
+}
 //Responsive
 window.addEventListener('resize', () => {
     camera.aspect = container.clientWidth / container.clientHeight;
@@ -64,14 +143,25 @@ window.addEventListener('resize', () => {
 })
 
 //Canvas element je nach dem ob es fokussiert wird klasse hinzufügen oder removen
-focusCanvasElement = () =>{
-    productContainer.addEventListener('focus', () =>{
+focusCanvasElement = () => {
+    productContainer.addEventListener('focus', () => {
         productContainer.classList.add('blurOuterContainer');
         product.classList.add('focusCanvas');
     })
-    productContainer.addEventListener('blur', () =>{
+    productContainer.addEventListener('blur', () => {
         productContainer.classList.remove('blurOuterContainer');
         product.classList.remove('focusCanvas');
     })
 }
 focusCanvasElement();
+
+//jeweilige Hotspots je nach Ansicht anzeigen lassen
+showHotspot = (hotspotNumber) => {
+    for (let i = 5; i <= 10; i++) {
+        if (i == hotspotNumber) {
+            shoe.parent.children[i].material.visible = true;
+        } else {
+            shoe.parent.children[i].material.visible = false;
+        }
+    }
+}
